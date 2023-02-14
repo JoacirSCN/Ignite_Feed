99e798f1d18ea56c76c243623843a16507dc90fc
joacir <joacirprog@gmail.com>
2023-02-14 12:46:11 -0300

    Estrutura da aplicação concluida

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Joacir Sampaio"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-        "
-      />
+        <main>
+          <Post
+            author="Joacir Sampaio"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+          />
+
+          <Post
+            author="Joacir Sampaio"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+          />
+        </main>
+      </div>
     </div>
   );
 }
-
-
---
+++ src/components/Comment.jsx
@@ -0,0 +1,47 @@
+import styles from "./Comment.module.css";
+import { BsFillTrashFill } from 'react-icons/bs';
+import { TiThumbsUp } from 'react-icons/ti';
+import { Avatar } from "./Avatar";
+
+
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false}
+        src="https://github.com/maykbrito.png" 
+        alt="Foto do Perfil" 
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mayk Brito</strong>
+              <time
+                title="14 de Fevereiro às 10:18"
+                dateTime="2023-02-14 10:18:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <BsFillTrashFill size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <TiThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
